f69536d50c64857aa5c621a26284a0f48d99fb4e
bambriz <beambriz2017@gmail.com>
2022-03-28 04:47:32 +0000

    Added basic local E2EE messaging

---
+++ retriever.py
@@ -0,0 +1,33 @@
+import csv
+from cryptography.fernet import Fernet
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+#msg=requests.get("https://thingspeak.com/channels/1461170/field/1")
+msgList = []
+with open('encrypted_messages.csv', 'r') as f:
+	for row in reversed(list(csv.reader(f))):
+		msgList.append(''.join(row))
+	#msgList = list(reversed(list(csv.reader(f))))
+	#print(msgList)
+
+msg = msgList[0]
+#msg=msg.json()['feeds'][-1]['field1']
+password_provided = input("Provide the key : ")
+password = password_provided.encode()
+salt = b'salt_'
+kdf = PBKDF2HMAC(
+	algorithm=hashes.SHA256(),
+	length=32,
+	salt=salt,
+	iterations=100000,
+	backend=default_backend()
+)
+key = base64.urlsafe_b64encode(kdf.derive(password))
+f=Fernet(key)
+print("\nEncrypted text you've received :\n\n"+msg)
+msg=msg[2:-1]
+msg=bytes(msg,'utf-8')
+msg=f.decrypt(msg)
+print("\nThe Message sent was: \n\n"+str(msg)[2:-1])
